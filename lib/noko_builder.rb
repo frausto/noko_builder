c3f40c402c454fa04aa6a3ac0c8413791ee0d81a
frausto <nrfrausto@gmail.com>
2012-02-12 00:05:44 -0500

    remove unused code

--- lib/noko_builder.rb
+++ lib/noko_builder.rb
@@ -14,10 +14,9 @@ class NokoBuilder
   end
   
   def method_missing(id, *args, &block)
-    first_arg = args.shift || {}
-    options = first_arg.kind_of?(Hash) ? first_arg : {}
+    first_arg = args.shift
     
-    default = @overwrite[id.to_sym] || options[:default] || first_arg
+    default = @overwrite[id.to_sym] || first_arg
     default = args.unshift(default).compact.reject(&:blank?)
     @xml.send(id, *default, &block) if default.present? || block.present?
   end
